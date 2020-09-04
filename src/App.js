74cddc0f8bace6e9c8637a4fc81dab7ed411fefa
Ricqrdo <ricqrdo-@hotmail.com>
2020-09-04 16:51:34 -0500

    Files completed in react setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
+import { BookProvider } from "./contexts/BookContext";
+import { FavsProvider } from "./contexts/FavsContext";
+import Menu from "./containers//Menu/Menu";
+import BookList from "./containers//BookList/BookList";
+import FavBooksList from "./containers/FavBooksList/FavBooksList";
+import MobileMenu from "./components/MobileMenu/MobileMenu";
+import PanelAdd from "./components/PanelAdd/PanelAdd";
+import DesktopMenu from "./components/DesktopMenu/DesktopMenu";
+
+export default function App() {
+  const [menu, setMenu] = useState(false);
+  const [newItemPanel, setNewItemPanel] = useState(false);
+
+  const add = () => setNewItemPanel(true);
+  const onCancel = () => setNewItemPanel(false);
+
+  const showMenu = () => setMenu(true);
+  const hideMenu = () => setMenu(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BookProvider>
+      <FavsProvider>
+        <Router>
+          <>
+            <Menu showMenu={showMenu} />
+            <Route path="/" exact component={BookList} />
+            <Route path="/favlist" component={FavBooksList} />
+            <DesktopMenu hideMenu={hideMenu} add={add} />
+            {menu && <MobileMenu hideMenu={hideMenu} add={add} />}
+            {newItemPanel && (
+              <PanelAdd onCancel={onCancel} hideMenu={hideMenu} />
+            )}
+          </>
+        </Router>
+      </FavsProvider>
+    </BookProvider>
   );
 }
-
-export default App;
